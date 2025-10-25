0ad1adb63cf9dbbe4b121402c3e980e6c67da78c
viettienmilo <viettien.milo@gmail.com>
2025-10-25 07:48:11 +0700

    update Vans (local storage) and VanDetail

--- freeCodeCamp/van-life/src/pages/VanDetail.jsx
+++ freeCodeCamp/van-life/src/pages/VanDetail.jsx
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-
+import { PacmanLoader } from 'react-spinners';
 
 export default function VanDetail() {
     const { id } = useParams();
@@ -11,7 +11,6 @@ export default function VanDetail() {
             .then(res => res.json())
             .then(data => setVan(data.vans))
     }, [id]);
-    console.log(van)
 
     return (
         <div className='van-detail-container'>
@@ -25,7 +24,7 @@ export default function VanDetail() {
                         <p>{van.description}</p>
                         <button className='link-button'>Rent this van</button>
                     </div>
-                    : <h2>Loading...</h2>
+                    : <div className='data-loading'><PacmanLoader color="#E17654" size={40} /></div>
             }
         </div>
     );
--- freeCodeCamp/van-life/src/pages/Vans.jsx
+++ freeCodeCamp/van-life/src/pages/Vans.jsx
@@ -5,11 +5,19 @@ export default function Vans() {
 
     const [vans, setVans] = useState([]);
     useEffect(() => {
-        fetch("/api/vans")
-            .then(res => res.json())
-            .then(data => setVans(data.vans));
+        const cached = localStorage.getItem("vans");
+        if (cached) {
+            setVans(JSON.parse(cached));
+        }
+        else {
+            fetch("/api/vans")
+                .then(res => res.json())
+                .then(data => {
+                    setVans(data.vans);
+                    localStorage.setItem("vans", JSON.stringify(data.vans));
+                })
+        }
     }, []);
-
 
     return (
         <div className='van-list-container'>
